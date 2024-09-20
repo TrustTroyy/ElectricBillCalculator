1ac9aa57ef0724bafb29980838db6afa31a9796e
TrustTroyy <jwinston10@mail.valenciacollege.edu>
2024-09-19 22:10:56 -0400

    Calculate electric bill

--- electric_bill_calculator.py
+++ electric_bill_calculator.py
@@ -6,4 +6,5 @@ else:
     total_cost = 1000 * 0.07633
     total_cost += (kWh_used - 1000) * 0.09259
 
-print(f"Amount owed is ${total_cost:.2f}")
+print(f" Amount owed is ${total_cost:.2f}")
+#YEAH I DID THAT
