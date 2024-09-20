4dd42c90c3f3bbe0fa1fc99eb7b98cc2a137569c
TrustTroyy <jwinston10@mail.valenciacollege.edu>
2024-09-19 21:39:52 -0400

    Calculate electric bill

---
+++ electric_bill_calculator.py
@@ -0,0 +1,9 @@
+kWh_used = int(input("Enter the KW hours used: "))
+
+if kWh_used <= 1000:
+    total_cost = kWh_used * 0.07633
+else:
+    total_cost = 1000 * 0.07633
+    total_cost += (kWh_used - 1000) * 0.09259
+
+print(f"Amount owed is ${total_cost:.2f}")
